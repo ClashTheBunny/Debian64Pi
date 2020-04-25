6f8a16b981c3e899fd5353b2536e0463d924276c
Randall Mason <randall@mason.ch>
2020-04-25 10:17:49 -0500

    Modularize and Get jetson nano bootstrapping.
    * TODO seems like we run out of space on jetson-disk-image-creator.sh
      * This only affects filesystems that transparently compress and
        present the compressed space to `du -ms`.  Change it to whatever
        command tells you the actual space used in flash.sh if you need to.
    * Should we build or include libjpeg-turbo-dummy.deb?
      * I'm currently including both the file to build it and the dummy deb.
        The script uses the dummy deb.

--- stage1.sh
+++ stage1.sh
@@ -1,83 +1,240 @@
-# Setup ext4 root filesystem
+#!/usr/bin/env bash
 
-qemu-debootstrap --arch=arm64 buster /mnt
+set -euf -o pipefail
 
-mount -o bind /proc /mnt/proc
-mount -o bind /dev /mnt/dev
-mount -o bind /dev/pts /mnt/dev/pts
-mount -o bind /sys /mnt/sys
+MOUNTPOINT="${MOUNTPOINT:-/mnt}"
+GRAPHICAL="${GRAPHICAL:-true}"
+RELEASE="${RELEASE:-buster}"
+BOARD="${BOARD:-pi}"
+REVISION="${REVISION:-4}"
+SIZE="${3:-3G}"
 
-# Make internet available from within the chroot, and setup fstab, hostname, and sources.list
+if [[ $EUID -ne 0 ]]; then
+	SUDO="${SUDO:-sudo}"
+fi
 
-cp /etc/resolv.conf /mnt/etc/resolv.conf
-rm /mnt/etc/fstab
-rm /mnt/etc/hostname
-rm /mnt/etc/apt/sources.list
-echo "proc            /proc           proc    defaults          0       0
-/dev/mmcblk0p1  /boot           vfat    defaults          0       2
-/dev/mmcblk0p2  /               ext4    defaults,noatime  0       1" >> /mnt/etc/fstab
+function download_tegra_driver_package() {
+	wget -c https://developer.nvidia.com/embedded/L4T/r32_Release_v4.2/t210ref_release_aarch64/Tegra210_Linux_R32.4.2_aarch64.tbz2
+	# wget -c https://developer.nvidia.com/embedded/L4T/r32_Release_v4.2/t210ref_release_aarch64/Tegra_Linux_Sample-Root-Filesystem_R32.4.2_aarch64.tbz2
+	mkdir -p /tmp/jetson_driver_package/
+	tar xf Tegra210_Linux_R32.4.2_aarch64.tbz2 -C /tmp/jetson_driver_package/
+	# tar xf Tegra_Linux_Sample-Root-Filesystem_R32.4.2_aarch64.tbz2 -C /tmp/jetson_driver_package/rootfs/
+}
 
-echo "debian-rpi64" >> /mnt/etc/hostname
+function create_image_jetson-nano() {
 
-echo "deb http://deb.debian.org/debian/ buster main contrib non-free
-#deb-src http://deb.debian.org/debian/ buster main contrib non-free
+	download_tegra_driver_package
 
-deb http://deb.debian.org/debian/ buster-updates main contrib non-free
-#deb-src http://deb.debian.org/debian/ buster-updates main contrib non-free
+}
 
-deb http://deb.debian.org/debian-security buster/updates main contrib non-free
-#deb-src http://deb.debian.org/debian-security buster/updates main contrib non-free
+function finalize_image_jetson-nano() {
 
-deb http://ftp.debian.org/debian buster-backports main
-#deb-src http://ftp.debian.org/debian buster-backports main" >> /mnt/etc/apt/sources.list
+	$SUDO cp libjpeg-turbo-dummy_1.0_all.deb "${MOUNTPOINT}"
+	$SUDO chroot "${MOUNTPOINT}" dpkg -i libjpeg-turbo-dummy_1.0_all.deb
+	$SUDO rm "${MOUNTPOINT}/libjpeg-turbo-dummy_1.0_all.deb"
+	$SUDO chroot "${MOUNTPOINT}" apt -f install
 
-# Setup bootloader and kernel
+	(
+		cd /tmp/jetson_driver_package/Linux_for_Tegra/ || exit
 
-wget http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/raspberrypi-bootloader_1.20190925-2_armhf.deb
-mkdir /tmp/pi-bootloader/
-dpkg-deb -x raspberrypi-bootloader_1.20190925-2_armhf.deb /tmp/pi-bootloader/
-cp /tmp/pi-bootloader/boot/* /mnt/boot/
-rm raspberrypi-bootloader_1.20190925-2_armhf.deb
+		sudo cp "${MOUNTPOINT}/../nv_tegra/l4t_deb_packages/nvidia-l4t-init_32.4.2-20200408182156_arm64.deb" "${MOUNTPOINT}"
+		$SUDO chroot "${MOUNTPOINT}" dpkg -i --force-confnew --force-depends --force-overwrite /nvidia-l4t-init_32.4.2-20200408182156_arm64.deb
+		$SUDO rm "${MOUNTPOINT}/nvidia-l4t-init_32.4.2-20200408182156_arm64.deb"
 
-VERSION=4.19.113.20200414
-PATCH='-bis'
+		$SUDO chroot "${MOUNTPOINT}" groupdel trusty
+		$SUDO chroot "${MOUNTPOINT}" groupdel crypto
 
-wget https://github.com/sakaki-/bcm2711-kernel${PATCH}/releases/download/${VERSION}/bcm2711-kernel${PATCH}-${VERSION}.tar.xz
-mkdir /tmp/pi-kernel
-tar xf bcm2711-kernel${PATCH}-${VERSION}.tar.xz -C /tmp/pi-kernel/
-cp -r /tmp/pi-kernel/boot/* /mnt/boot/
-mv /mnt/boot/kernel*.img /mnt/boot/kernel8.img
-mkdir /mnt/lib/modules
-cp -r /tmp/pi-kernel/lib/modules /mnt/lib/
-rm bcm2711-kernel${PATCH}-${VERSION}.tar.xz
-rm -r /tmp/pi-kernel
+		$SUDO ./apply_binaries.sh
 
-## Comment or remove completely the above kernel setup and uncomment the kernel setup below to setup the image to run on the Pi 3 instead of the Pi 4.
+	)
 
-#wget https://github.com/sakaki-/bcmrpi3-kernel/releases/download/4.19.89.20191224/bcmrpi3-kernel-4.19.89.20191224.tar.xz
-#mkdir /tmp/pi-kernel
-#tar xf bcmrpi3-kernel-4.19.89.20191224.tar.xz -C /tmp/pi-kernel/
-#cp -r /tmp/pi-kernel/boot/* /mnt/boot/
-#mkdir /mnt/lib/modules
-#cp -r /tmp/pi-kernel/lib/modules /mnt/lib/
-#rm bcmrpi3-kernel-4.19.89.20191224.tar.xz
-#rm -r /tmp/pi-kernel
+	chroot_tear_down
 
-# Setup config.txt and cmdline.txt
+	$SUDO /tmp/jetson_driver_package/Linux_for_Tegra/tools/jetson-disk-image-creator.sh -o "debian-${BOARD}.img" -b "${BOARD}" -r "$REVISION"
 
-echo "[pi4]
-# Enable DRM VC4 V3D driver on top of the dispmanx display stack
-dtoverlay=vc4-fkms-v3d
-max_framebuffers=2
-arm_64bit=1
-# differentiate from Pi3 64-bit kernels
-kernel=kernel8-p4.img" >> /mnt/boot/config.txt
+}
 
-echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait" >> /mnt/boot/cmdline.txt
+function create_image_pi() {
+	if [[ -f debian-rpi64.img ]]; then
+		read -p "debian-rpi64.img already exists, overwrite and start over? " -r yn
+		case $yn in
+		[Nn]*)
+			exit
+			;;
+		esac
+	fi
+	qemu-img create debian-rpi64.img "${SIZE}"
 
-# Copy stage2 to chroot environment
+	LOOPDEV=$($SUDO losetup -f -P --show debian-rpi64.img)
 
-cp stage2.sh /mnt/
-echo "Run stage2.sh in chroot for stage 2."
-chroot /mnt /bin/bash /stage2.sh
-./stage3.sh
+	if [[ $GRAPHICAL == true ]]; then
+		$SUDO gparted "$LOOPDEV"
+	else
+		$SUDO fdisk "$LOOPDEV"
+		$SUDO mkfs.fat -F32 -v -I -n'BOOT' "${LOOPDEV}p1"
+		$SUDO mkfs.ext4 -F -O '^64bit' -L 'root' "${LOOPDEV}p2"
+	fi
+
+	$SUDO mount "${LOOPDEV}p2" "${MOUNTPOINT}"
+	$SUDO mkdir -p "${MOUNTPOINT}/boot"
+	$SUDO mount "${LOOPDEV}p1" "${MOUNTPOINT}/boot"
+
+}
+
+function finalize_image_pi() {
+	# Install Pi-compatible WiFi drivers to image
+
+	mkdir -p wifi-firmware
+	(
+		cd wifi-firmware || exit
+		wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43430-sdio.bin
+		wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43430-sdio.clm_blob
+		wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43430-sdio.txt
+		wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43455-sdio.bin
+		wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43455-sdio.clm_blob
+		wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43455-sdio.txt
+		cp ./*sdio* "${MOUNTPOINT}/lib/firmware/brcm/"
+	)
+	rm -rf wifi-firmware
+
+	# Remove stage2.sh from root and unmount filesystem and mount points
+
+	rm "${MOUNTPOINT}/stage2.sh"
+	chroot_tear_down
+}
+
+function base_bootstrap() {
+
+	$SUDO qemu-debootstrap --arch=arm64 "${RELEASE}" "${MOUNTPOINT}"
+
+	# Make internet available from within the chroot, and setup fstab, hostname, and sources.list
+
+	$SUDO cp /etc/resolv.conf "${MOUNTPOINT}/etc/resolv.conf"
+	$SUDO rm "${MOUNTPOINT}/etc/fstab"
+	$SUDO rm "${MOUNTPOINT}/etc/hostname"
+	$SUDO rm "${MOUNTPOINT}/etc/apt/sources.list"
+	echo "proc /proc proc defaults 0 0
+	/dev/mmcblk0p1 /boot vfat defaults 0 2
+	/dev/mmcblk0p2 / ext4 defaults,noatime 0 1" | $SUDO tee -a "${MOUNTPOINT}/etc/fstab"
+
+	echo "debian-rpi64" | $SUDO tee "${MOUNTPOINT}/etc/hostname"
+
+	echo "deb http://deb.debian.org/debian/ ${RELEASE} main contrib non-free
+	#deb-src http://deb.debian.org/debian/ ${RELEASE} main contrib non-free
+
+	deb http://deb.debian.org/debian/ ${RELEASE}-updates main contrib non-free
+	#deb-src http://deb.debian.org/debian/ ${RELEASE}-updates main contrib non-free
+
+	deb http://deb.debian.org/debian-security ${RELEASE}/updates main contrib non-free
+	#deb-src http://deb.debian.org/debian-security ${RELEASE}/updates main contrib non-free
+
+	deb http://ftp.debian.org/debian ${RELEASE}-backports main
+	#deb-src http://ftp.debian.org/debian ${RELEASE}-backports main" | $SUDO tee -a "${MOUNTPOINT}/etc/apt/sources.list"
+
+}
+
+function setup_pi() {
+	# Setup bootloader and kernel
+
+	BOOT_VERSION="1.20200212-1"
+	BOOT_ARCH="armhf"
+
+	wget -c "http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/raspberrypi-bootloader_${BOOT_VERSION}_${BOOT_ARCH}.deb"
+	mkdir -p /tmp/pi-bootloader/
+	dpkg-deb -x "raspberrypi-bootloader_${BOOT_VERSION}_${BOOT_ARCH}.deb" /tmp/pi-bootloader/
+	$SUDO cp /tmp/pi-bootloader/boot/ "${MOUNTPOINT}/boot/"
+	rm "raspberrypi-bootloader_${BOOT_VERSION}_${BOOT_ARCH}.deb"
+
+	if [[ $1 == 4 ]]; then
+
+		VERSION='4.19.115.20200421'
+		PATCH='-bis'
+
+		wget -c https://github.com/sakaki-/bcm2711-kernel${PATCH}/releases/download/${VERSION}/bcm2711-kernel${PATCH}-${VERSION}.tar.xz
+		mkdir /tmp/pi-kernel
+		tar xf bcm2711-kernel${PATCH}-${VERSION}.tar.xz -C /tmp/pi-kernel/
+		$SUDO cp -r /tmp/pi-kernel/boot/ "${MOUNTPOINT}/boot/"
+		$SUDO mv "${MOUNTPOINT}/boot/kernel*.img" "${MOUNTPOINT}/boot/kernel8.img"
+		$SUDO mkdir "${MOUNTPOINT}/lib/modules"
+		$SUDO cp -r /tmp/pi-kernel/lib/modules "${MOUNTPOINT}/lib/"
+		rm bcm2711-kernel${PATCH}-${VERSION}.tar.xz
+		rm -r /tmp/pi-kernel
+
+		echo "[pi4]
+    # Enable DRM VC4 V3D driver on top of the dispmanx display stack
+    dtoverlay=vc4-fkms-v3d
+    max_framebuffers=2
+    arm_64bit=1
+    # differentiate from Pi3 64-bit kernels
+    kernel=kernel8-p4.img" | $SUDO tee -a "${MOUNTPOINT}/boot/config.txt"
+
+		echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait" | $SUDO tee -a "${MOUNTPOINT}/boot/cmdline.txt"
+
+	elif [[ $1 == 3 ]]; then
+
+		wget -c https://github.com/sakaki-/bcmrpi3-kernel/releases/download/4.19.89.20191224/bcmrpi3-kernel-4.19.89.20191224.tar.xz
+		mkdir /tmp/pi-kernel
+		tar xf bcmrpi3-kernel-4.19.89.20191224.tar.xz -C /tmp/pi-kernel/
+		cp -r /tmp/pi-kernel/boot/ "${MOUNTPOINT}/boot/"
+		mkdir "${MOUNTPOINT}/lib/modules"
+		cp -r /tmp/pi-kernel/lib/modules "${MOUNTPOINT}/lib/"
+		rm bcmrpi3-kernel-4.19.89.20191224.tar.xz
+		rm -r /tmp/pi-kernel
+
+		echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait" | $SUDO tee -a "${MOUNTPOINT}/boot/cmdline.txt"
+
+	fi
+}
+
+function setup_jetson-nano() {
+	true
+}
+
+function chroot_prepare() {
+	$SUDO mount -o bind /proc "${MOUNTPOINT}/proc"
+	$SUDO mount -o bind /dev "${MOUNTPOINT}/dev"
+	$SUDO mount -o bind /dev/pts "${MOUNTPOINT}/dev/pts"
+	$SUDO mount -o bind /sys "${MOUNTPOINT}/sys"
+}
+
+function chroot_tear_down() {
+	$SUDO umount "${MOUNTPOINT}/proc"
+	$SUDO umount "${MOUNTPOINT}/sys"
+	$SUDO umount "${MOUNTPOINT}/dev/pts"
+	$SUDO umount "${MOUNTPOINT}/dev" -l
+	$SUDO umount "${MOUNTPOINT}/boot"
+	$SUDO umount "${MOUNTPOINT}" -l
+}
+
+function main() {
+
+	"create_image_${BOARD}"
+
+	base_bootstrap "${MOUNTPOINT}"
+
+	"setup_${BOARD}" "${REVISION}"
+
+	# Copy stage2 to chroot environment and execute
+
+	$SUDO cp stage2.sh "${MOUNTPOINT}/"
+
+	echo "Runing stage2.sh in chroot."
+	chroot_prepare
+	$SUDO chroot "${MOUNTPOINT}" /bin/bash /stage2.sh
+
+	# run stage3 outside of chroot
+	"finalize_image_${BOARD}"
+}
+
+set +u
+
+([[ -n $ZSH_EVAL_CONTEXT && $ZSH_EVAL_CONTEXT =~ :file$ ]] ||
+	[[ -n $BASH_VERSION ]] && (return 0 2>/dev/null)) && sourced=true || sourced=false
+
+if $sourced; then
+	echo "Script is being sourced; functions now available at the command line"
+	set +euf +o pipefail
+else
+	set -u
+	main
+fi
--- stage2.sh
+++ stage2.sh
@@ -1,9 +1,38 @@
 # Apt install core packages
 
+echo "locales locales/default_environment_locale select en_US.UTF-8
+locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8
+keyboard-configuration  keyboard-configuration/variantcode      string
+keyboard-configuration  keyboard-configuration/xkb-keymap       select  us
+keyboard-configuration  keyboard-configuration/toggle   select  No toggling
+# Keep current keyboard options in the configuration file?
+keyboard-configuration  keyboard-configuration/unsupported_config_options       boolean true
+keyboard-configuration  keyboard-configuration/layoutcode       string  us
+keyboard-configuration  keyboard-configuration/store_defaults_in_debconf_db     boolean true
+keyboard-configuration  keyboard-configuration/switch   select  No temporary switch
+keyboard-configuration  keyboard-configuration/variant  select  English (US)
+# Country of origin for the keyboard:
+keyboard-configuration  keyboard-configuration/layout   select
+keyboard-configuration  keyboard-configuration/model    select  Generic 105-key PC (intl.)
+# Keep default keyboard options ()?
+keyboard-configuration  keyboard-configuration/unsupported_options      boolean true
+keyboard-configuration  keyboard-configuration/ctrl_alt_bksp    boolean false
+keyboard-configuration  keyboard-configuration/modelcode        string  pc105
+# Keep default keyboard layout ()?
+keyboard-configuration  keyboard-configuration/unsupported_layout       boolean true
+# Choices: The default for the keyboard layout, No AltGr key, Right Alt (AltGr), Right Control, Right Logo key, Menu key, Left Alt, Left Logo key, Keypad Enter key, Both Logo keys, Both Alt keys
+keyboard-configuration  keyboard-configuration/altgr    select  The default for the keyboard layout
+# Keep the current keyboard layout in the configuration file?
+keyboard-configuration  keyboard-configuration/unsupported_config_layout        boolean true
+keyboard-configuration  keyboard-configuration/compose  select  No compose key
+keyboard-configuration  keyboard-configuration/optionscode      string" | debconf-set-selections
+
 apt update
-apt upgrade -y 
+apt install locales -y
+apt upgrade -y
 apt install console-setup keyboard-configuration sudo ssh curl wget dbus usbutils ca-certificates crda less fbset debconf-utils avahi-daemon fake-hwclock nfs-common apt-utils man-db pciutils ntfs-3g apt-listchanges -y
 apt install wpasupplicant wireless-tools firmware-atheros firmware-brcm80211 firmware-libertas firmware-misc-nonfree firmware-realtek dhcpcd5 net-tools -y
+apt install device-tree-compiler fontconfig fontconfig-config fonts-dejavu-core libcairo2 libdatrie1 libfontconfig1 libfreetype6 libfribidi0 libgles2 libglib2.0-0 libglib2.0-data libgraphite2-3 libharfbuzz0b libpango-1.0-0 libpangoft2-1.0-0 libpixman-1-0 libpng16-16 libthai-data libthai0 libxcb-render0 libxcb-shm0 libxrender1 shared-mime-info xdg-user-dirs libdrm-common libdrm2 libegl-mesa0 libegl1 libgbm1 libglapi-mesa libglvnd0 libwayland-client0 libwayland-server0 libx11-xcb1 libxcb-dri2-0 libxcb-dri3-0 libxcb-present0 libxcb-sync1 libxcb-xfixes0 libxshmfence1
 
 # Uncomment one of these to install a Desktop Environment
 
@@ -15,8 +44,10 @@ apt install wpasupplicant wireless-tools firmware-atheros firmware-brcm80211 fir
 #apt install task-lxqt-desktop -y # For LXQT
 
 # Setup default user; this step does require a bit of user interaction for password and user info
+# shellcheck disable=SC2016
 useradd -m -p '$6$bN7eXEuAm6uMIuX2$m760DygUoCHMA3yc2.cKfFMBFxvGeuBWfPM13FV5TB69IHjS1TDCPVP6X9xrQrHHvvkm99aVpi4B94byyPHNO/' -s /bin/bash pi # pi / raspberry
 usermod -aG sudo,video,audio,cdrom pi
 
-useradd -m -p '$6$bESbi8ENieH9wyMV$m/4ZAjsFJxdYGhp29MO/haSCfjv2Y6IBuBYa.2up8QhNTA2xevXHEFQj16xWDDGC3EtdgqlX6B1fiT/3FUJ8P1' -s /bin/bash debian  # debian / debian
+# shellcheck disable=SC2016
+useradd -m -p '$6$bESbi8ENieH9wyMV$m/4ZAjsFJxdYGhp29MO/haSCfjv2Y6IBuBYa.2up8QhNTA2xevXHEFQj16xWDDGC3EtdgqlX6B1fiT/3FUJ8P1' -s /bin/bash debian # debian / debian
 usermod -aG sudo,video,audio,cdrom debian

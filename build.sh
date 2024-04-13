fe57b713c1d7785a494abc0271046f8c57ef7d7c
Randall Mason <Randall@Mason.CH>
2024-04-13 15:30:30 +0000

    Swap order, -v detects presence, not absence

--- build.sh
+++ build.sh
@@ -67,11 +67,11 @@ function base_bootstrap() {
 	$SUDO rm "${MOUNTPOINT}/etc/hostname"
 	$SUDO rm "${MOUNTPOINT}/etc/apt/sources.list"
 	if [[ -v LOOPDEV ]]; then
+		ROOTUUID=$($SUDO blkid -s UUID -o export "${LOOPDEV}p2" | grep UUID)
+		BOOTUUID=$($SUDO blkid -s UUID -o export "${LOOPDEV}p1" | grep UUID)
+	else
 		ROOTUUID=/dev/mmcblk0p2
 		BOOTUUID=/dev/mmcblk0p1
-	else
-		ROOTUUID=$($SUDO blkid -s UUID -o export "${LOOPDEV:-error}p2" | grep UUID)
-		BOOTUUID=$($SUDO blkid -s UUID -o export "${LOOPDEV:-error}p1" | grep UUID)
 	fi
 	echo "proc /proc proc defaults 0 0
 	$BOOTUUID /boot vfat defaults 0 2
